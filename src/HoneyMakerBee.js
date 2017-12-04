d557dbb756fa0e13046b3d7f0c238b9ae3854e1b
Mark Perry <mark.j.perry11@gmail.com>
2017-12-04 11:12:25 -0800

    Add honeymakerbee

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,17 @@
-class HoneyMakerBee {
+class HoneyMakerBee extends Bee {
+  constructor() {
+    super();
+    this.age = 10;
+    this.job = 'make honey';
+    this.color = 'yellow';
+    this.honeyPot = 0;
+  }
+
+  makeHoney() {
+    this.honeyPot+=1;
+  }
+  giveHoney() {
+    this.honeyPot-=1;
+  }
   // TODO..
 };
